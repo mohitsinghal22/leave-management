e492349932482584c9c4bb840c2d6bec5f8d78b9
pranjal161 <161pranjal@gmail.com>
2021-08-25 17:48:14 +0530

    CSS fixes

--- Frontend/src/components/Homepage.js
+++ Frontend/src/components/Homepage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { User, Admin } from './CSS/svg';
 
 export default function Homepage() {
     return (
         <div>
-            <ul>
-                <li><Link to='/employee'>Employee</Link></li>
-                <li><Link to='/admin'>Admin</Link></li>
+            <ul className="home-page">
+                <li><Link to='/employee'><div>{<User />}</div>Employee</Link></li>
+                <li><Link to='/admin'><div>{<Admin />}</div>Admin</Link></li>
             </ul>
         </div>
     )
--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Home } from './CSS/svg'
+import './CSS/Navbar.css';
+import {Link} from 'react-router-dom'
 
 export default function Navbar() {
     return (
         <header>
-            <h1>Employee leave Tracker</h1>
+            <div style={{ display: 'flex' }}>
+                <div className="header-flex--left"><Link to='/'>{<Home />}</Link></div>
+                <div style={{ width: "100%" }}><h1>Employee leave Tracker</h1></div>
+            </div>
         </header>
     )
 }

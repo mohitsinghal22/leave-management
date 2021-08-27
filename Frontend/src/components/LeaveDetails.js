0614fbe0614cb6c2963ffa167ab755e7dcc1b8b5
vivek-Madaram <vivek.madaram@gmail.com>
2021-08-27 16:19:13 +0530

    added date format

--- Frontend/src/components/LeaveDetails.js
+++ Frontend/src/components/LeaveDetails.js
@@ -1,106 +1,43 @@
 import React, { Component } from 'react';
 import XLSX from 'xlsx';
 import { saveAs } from 'file-saver';
-let Leavesdata = [
-   {
-      employeeId: '20015548',
-      PSID: '3687419',
-      employeeName: 'Emp1',
-      startDate: '2021-08-26',
-      endDate: '2021-08-27',
-   },
-   {
-      employeeId: '20015552',
-      PSID: '3687465',
-      employeeName: 'Emp2',
-      startDate: '2021-08-28',
-      endDate: '2021-08-29',
-   },
-   {
-      employeeId: '20015575',
-      PSID: '3687492',
-      employeeName: 'Emp3',
-      startDate: '2021-08-01',
-      endDate: '2021-08-02',
-   },
-   {
-      employeeId: '20015554',
-      PSID: '3687487',
-      employeeName: 'Emp4',
-      startDate: '2021-08-03',
-      endDate: '2021-08-08',
-   },
-   {
-      employeeId: '20015500',
-      PSID: '3687466',
-      employeeName: 'Emp5',
-      startDate: '2021-08-05',
-      endDate: '2021-08-06',
-   },
-   {
-      employeeId: '2001550047',
-      PSID: '3687365',
-      employeeName: 'Emp6',
-      startDate: '2021-08-07',
-      endDate: '2021-08-08',
-   },
-   {
-      employeeId: '20015333',
-      PSID: '3681258',
-      employeeName: 'Emp7',
-      startDate: '2021-08-09',
-      endDate: '2021-08-10',
-   },
-   {
-      employeeId: '20012388',
-      PSID: '3682222',
-      employeeName: 'Emp8',
-      startDate: '2021-08-11',
-      endDate: '2021-08-12',
-   },
-   {
-      employeeId: '20012355',
-      PSID: '3681255',
-      employeeName: 'Emp9',
-      startDate: '2021-08-13',
-      endDate: '2021-08-14',
-   },
-   {
-      employeeId: '20015285',
-      PSID: '3645698',
-      employeeName: 'Emp10',
-      startDate: '2021-08-15',
-      endDate: '2021-08-16',
-   },
-];
+import format from 'date-fns/format';
+import axios from 'axios';
 
+const getDate = (value) => {
+   const date = value && new Date(value);
+   return date ? format(date, 'yyyy-MM-dd') : '';
+};
 export class Leavedetails extends Component {
    constructor(props) {
       super(props);
       this.state = {
          filteredEmpDetails: [],
 
-         startDate: new Date(),
-         endDate: new Date(),
+         startDate: '',
+         endDate: '',
       };
    }
+
+   componentDidMount() {
+      axios.get(`http://localhost:5000/employees/`).then((res) => {
+         this.setState({ employeeLeaveDetails: res.data });
+      });
+   }
    handleStartDate = (e) => {
-      this.setState({ startDate: e.target.value });
-      console.log(this.state.startDate);
+      this.setState({ startDate: getDate(e.target.value) });
    };
    handleEndDate = (e) => {
-      this.setState({ endDate: e.target.value });
-      console.log(this.state.endDate);
+      this.setState({ endDate: getDate(e.target.value) });
    };
+
    checkValues = (e) => {
       e.preventDefault();
-      console.log(this.state.startDate);
-      console.log(this.state.endDate);
 
       if (this.state.endDate <= this.state.startDate) {
          alert('Enter valid Start and End Dates');
       }
-      var result = Leavesdata.filter((obj) => {
+      var result = this.state.employeeLeaveDetails.filter((obj) => {
          return (
             (obj.startDate >= this.state.startDate &&
                obj.startDate <= this.state.endDate) ||
@@ -170,6 +107,7 @@ export class Leavedetails extends Component {
                            <th>Employee Name</th>
                            <th>Start Date</th>
                            <th>End Date</th>
+                           <th>Applied Leaves</th>
                         </tr>
                      </thead>
                      <tbody>
@@ -179,8 +117,9 @@ export class Leavedetails extends Component {
                                  <td>{employee.employeeId}</td>
                                  <td>{employee.PSID}</td>
                                  <td>{employee.employeeName}</td>
-                                 <td>{employee.startDate}</td>
-                                 <td>{employee.endDate}</td>
+                                 <td>{getDate(employee.startDate)}</td>
+                                 <td>{getDate(employee.endDate)}</td>
+                                 <td>{employee.appliedLeaves}</td>
                               </tr>
                            )
                         )}
